8e97d62ca803ee32f393a9bb7f08201b560e4176
Andrew <andrewscottpowers@gmail.com>
2018-08-15 20:04:30 -0700

    all 3 images up and replaced by 3 new images

--- app.js
+++ app.js
@@ -100,49 +100,77 @@ Product.imageArray = [];
 
 //these line below intialize new Products, which (populates) the objects with the 
 //cont... properties of the Product Constructor function above
-var productOne = new Product = ('img/assets/bag.jpg');
-var productTwo = new Product = ('img/assets/banana.jpg');
-var productThree = new Product = ('img/assets/bathroom.jpg');
-new Product = ('img/assets/boots.jpg');
-new Product = ('img/assets/breakfast.jpg');
-new Product = ('img/assets/bubblegum.jpg');
-new Product = ('img/assets/chair.jpg');
-new Product = ('img/assets/cthulhu.jpg');
-new Product = ('img/assets/dog-duck.jpg');
-new Product = ('img/assets/dragon.jpg');
-new Product = ('img/assets/pen.jpg');
-new Product = ('img/assets/pet-sweep.jpg');
-new Product = ('img/assets/scissors.jpg');
-new Product = ('img/assets/shark.jpg');
-new Product = ('img/assets/sweep.png');
-new Product = ('img/assets/tauntaun.jpg');
-new Product = ('img/assets/usb.gif');
-new Product = ('img/assets/usb.gif');
-new Product = ('img/assets/water-can.jpg');
-new Product = ('img/assets/wine-glass.jpg');
+var productOne = new Product('img/assets/bag.jpg');
+var productTwo = new Product ('img/assets/banana.jpg');
+var productThree = new Product ('img/assets/bathroom.jpg');
+new Product ('img/assets/boots.jpg');
+new Product ('img/assets/breakfast.jpg');
+new Product ('img/assets/bubblegum.jpg');
+new Product ('img/assets/chair.jpg');
+new Product ('img/assets/cthulhu.jpg');
+new Product ('img/assets/dog-duck.jpg');
+new Product ('img/assets/dragon.jpg');
+new Product ('img/assets/pen.jpg');
+new Product ('img/assets/pet-sweep.jpg');
+new Product ('img/assets/scissors.jpg');
+new Product ('img/assets/shark.jpg');
+new Product ('img/assets/sweep.png');
+new Product ('img/assets/tauntaun.jpg');
+new Product ('img/assets/usb.gif');
+new Product ('img/assets/usb.gif');
+new Product ('img/assets/water-can.jpg');
+new Product ('img/assets/wine-glass.jpg');
 
 //this function is created to make sure the random pictures are created
 //cont.. and that the next pictures selected are not the same as the previous
 //cont... or the same as each other
 
-function RandomNumberGen(){
+function displayingRandomImg() {
+
   var firstIndex = Math.floor(Math.random() * Product.imageArray.length);
   //this line creates a new var that has the [index number] of image array within the Product
   productOne = Product.imageArray[firstIndex];
   // do a test
   do {
     //create a var that holds the index location of the 2nd product img
-    var secondImgindex =  Math.floor(Math.random() * Product.imageArray.length);
+    var secondImgIndex =  Math.floor(Math.random() * Product.imageArray.length);
+    var thirdImgIndex =  Math.floor(Math.random() * Product.imageArray.length);
     // while the first index and second index are the same continue do-ing wthe line above
-  } while (secondImgIndex === firstIndex)
+  } while (firstIndex === secondImgIndex || firstIndex === thirdImgIndex || secondImgIndex === thirdImgIndex);
   // sett the new 
-  productTwo = Product.imageArray[secondImgindex];
-  
-  do {
-    //create a var that holds the index location of the 2nd product img
-    var thirdImgindex =  Math.floor(Math.random() * Product.imageArray.length);
-  } while (thirdImgIndex === firstIndex || thirdImgindex === firstIndex)
 
-  productThree = Product.imageArray[thirdImgindex];
 
+  productTwo = Product.imageArray[secondImgIndex];
+  productThree = Product.imageArray[thirdImgIndex];
+
+
+  //need to store in img.src --> for all the 
+  img1.src = productOne.filename;
+  img2.src = productTwo.filename;
+  img3.src = productThree.filename;
 }
+
+// do {
+//   //create a var that holds the index location of the 2nd product img
+//   var thirdImgindex =  Math.floor(Math.random() * Product.imageArray.length);
+// } while (thirdImgIndex === firstIndex || thirdImgindex === secondImgIndex)
+// productThree = Product.imageArray[thirdImgindex];
+
+var img1 = document.getElementsByTagName('img')[0];
+var img2 = document.getElementsByTagName('img')[1];
+var img3 = document.getElementsByTagName('img')[2];
+
+img1.addEventListener('click', function() {
+  productOne.votes++;
+  displayingRandomImg();
+});
+
+img2.addEventListener('click', function() {
+  productTwo.votes++;
+  displayingRandomImg();
+});
+
+img3.addEventListener('click', function() {
+  productThree.votes++;
+  displayingRandomImg();
+});
